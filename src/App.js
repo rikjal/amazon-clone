9a03e3662a28933b26f92a74e56e178545094456
rikjal <saptarshi.jpg@gmail.com>
2020-09-18 15:50:56 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit React App
+          Edit React App with VSCode
         </p>
         <a
           className="App-link"
